6d60737ef395ffabf7156f1e41f698d928feb845
Leonardo Ferreira de Abreu <leonardofabreu@hotmail.com>
2022-07-27 11:07:27 -0300

    adiciona requisito 11

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// const subTotal = document.querySelector('.total-price');
 const clearCartButton = document.querySelector('.empty-cart');
+// const cartClass = document.querySelector('.cart__items');
+const [loadingElement] = document.getElementsByClassName('loading');
 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
@@ -26,8 +29,20 @@ const createCustomElement = (element, className, innerText) => {
   return section;
 };
 
+// const updateSubtotal = async () => {
+//   const price = [...document.getElementsByClassName('cart__item')]
+//     .map((element) => element.innerText)
+//     .map((content) => content.match(/\$(\d+\.?\d+)/m)[1])
+//     .map((stringNumber) => Number(stringNumber))
+//     .reduce((total, current) => total + current, 0);
+
+//   // subtotalElement.innerText = price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+//   subTotal.innerText = price;
+// };
+
 const cartItemClickListener = (event) => {
   event.target.remove();
+  // updateSubtotal();
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
@@ -35,8 +50,9 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  // updateSubtotal();
   return li;
-};
+ };
 
 const addItemCart = async (product) => {
   const data = await fetchItem(product);
@@ -45,13 +61,10 @@ const addItemCart = async (product) => {
     const name = data.title;
     const salePrice = data.price;
     const item = document.querySelector('.cart__items');
+    // getLoadingMsg();
   // console.log(item);
 
   item.appendChild(createCartItemElement({ sku, name, salePrice }));
-  
-  // createCartItemElement({ sku, name, salePrice });
-  
-  // });
 };
 
 function addEventButton(event) {
@@ -93,5 +106,19 @@ const clearCartItems = () => {
   ol.innerHTML = '';
 };
 
-window.onload = () => { };
+const sendProducts = async () => {
+  const { results: products } = await fetchProducts('computador');
+  loadingElement.remove();
+
+  products.forEach(({ id: sku, title: name, thumbnail: image }) => {
+    productListElement.appendChild(createProductItemElement({ sku, name, image }));
+  });
+  Array.from(document.getElementsByClassName('item__add')).forEach((element) => {
+    element.addEventListener('click', addItemCart);
+  });
+};
+
+window.onload = async () => {
 clearCartButton.addEventListener('click', clearCartItems);
+sendProducts();
+};
